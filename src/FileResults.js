d32ef537f68fb4e2541ab377d08d91657f701cf5
Elena Smith <elenasmith@Elenas-MacBook-Air.local>
2020-12-29 15:41:00 -0600

    Rendered header from state

--- src/FileResults.js
+++ src/FileResults.js
@@ -1,8 +1,10 @@
 import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
+import TableHeader from "./TableHeader";
 
 export default class FileResults extends PureComponent {
   static propTypes = {
+    header: PropTypes.array,
     data: PropTypes.array
   };
 
@@ -16,19 +18,19 @@ export default class FileResults extends PureComponent {
     return (
       <div>
       <table class="table">
-        <thead>
-        <tr>
-           {
-                this.props.data[0].map(headerTitle =>(
-                   <td>
-                    {
-                      headerTitle
-                    }
-                   </td>
-                ))
-           }
-           </tr>
-        </thead>
+      <thead>
+      <tr>
+         {
+              this.props.header.map(headerTitle =>(
+                 <td>
+                  {
+                    headerTitle
+                  }
+                 </td>
+              ))
+         }
+         </tr>
+      </thead>
         {
           this.props.data.slice(1, this.props.data.length).map(dataRow => (
             <tr>
