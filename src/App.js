27a12234e028a221bbc8e0d62d1606d524456cbd
Elena Smith <elenasmith@Elenas-MacBook-Air.local>
2020-12-30 09:36:02 -0600

    Wrote a setState change on button click that re-renders App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from "react";
+import React, { PureComponent, useState} from "react";
 import FileResults from "./FileResults";
 import Header from "./Header";
 import CSVReader from 'react-csv-reader';
@@ -14,10 +14,6 @@ export default class App extends PureComponent {
   }
 
   handleForce = (data, fileInfo) => {
-    console.log(data);
-    console.log(fileInfo);
-    console.log('header');
-    console.log(data[0]);
     this.setState({
       header: data[0],
       data: data.slice(1,data.length),
@@ -25,11 +21,39 @@ export default class App extends PureComponent {
     })
   }
 
+  handleButton = () => {
+
+    this.setState({
+      data: [[]],
+      fileInfo: undefined,
+      header: [],
+    })
+
+    // let original_header = this.state.header;
+    // let original_data = this.state.data;
+    //
+    // let index=0;
+    // for (index = 0; index < original_data.length; index++) {
+    //   original_data[index].push("");
+    // }
+    // original_header.push('Category');
+    //
+    // //not erroring out but not re-rendering table
+    // this.setState({
+    //   header: original_header,
+    //   data: original_data,
+    //   fileInfo: this.state.fileInfo,
+    // })
+
+  }
+
 
   render() {
+    console.log(this.state);
     return (
       <div>
         <Header />
+        <div><button onClick={this.handleButton}>Add Column</button></div>
         <CSVReader onFileLoaded={this.handleForce} />
         <FileResults data={this.state.data} header={this.state.header}/> {/*Props are passed to components via HTML attributes.*/}
       </div>
