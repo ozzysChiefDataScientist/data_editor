8f38959dde7849e5ff38f1bc08e578aa2c437c35
Elena Smith <elenasmith@Elenas-MacBook-Air.local>
2021-01-02 11:43:22 -0600

    Removed unused import

--- src/FileResults.js
+++ src/FileResults.js
@@ -1,6 +1,5 @@
 import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
-import TableHeader from "./TableHeader";
 import ReactTable from "react-table";
 import TableCell from "./TableCell";
 
